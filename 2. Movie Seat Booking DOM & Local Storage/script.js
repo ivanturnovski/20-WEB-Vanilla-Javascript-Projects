4d9782e09cddaa4c4819e93a5a1d105ea0eca9f1
Ivan Turnovski <ivanturnovski@gmail.com>
2020-09-20 05:16:31 +0200

    Movie Seat Booking - Save Data to Local Storage

--- 2. Movie Seat Booking DOM & Local Storage/script.js
+++ 2. Movie Seat Booking DOM & Local Storage/script.js
@@ -6,22 +6,37 @@ const movieSelect = document.getElementById('movie');
 
 let ticketPrice = movieSelect.value;
 
+//Save selected movie index and price
+
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem('selectedMovieIndex', movieIndex);
+  localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+
 //Update total and count
-
-//Movie select event
-
-movieSelect.addEventListener('change', (e) => {
-  ticketPrice = +e.target.value;
-  updateSelectedCount();
-});
-//Seat click event
 function updateSelectedCount() {
   const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+  const seatsIndex = [...selectedSeats].map((seat) => {
+    return [...seats].indexOf(seat);
+  });
+
+  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
   const selectedSeatsCount = selectedSeats.length;
   console.log(selectedSeatsCount);
   count.innerText = selectedSeatsCount;
   total.innerText = selectedSeatsCount * ticketPrice;
 }
+
+//Movie select event
+movieSelect.addEventListener('change', (e) => {
+  ticketPrice = +e.target.value;
+  setMovieData(e.target.selectedIndex, e.target.value);
+  updateSelectedCount();
+});
+
+//Seat click event
 container.addEventListener('click', (e) => {
   if (
     e.target.classList.contains('seat') &&
