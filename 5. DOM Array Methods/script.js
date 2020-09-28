70b226b6f0adb3ae69dfe41212ed632830a67bb4
Ivan Turnovski <ivanturnovski@gmail.com>
2020-09-28 19:06:41 +0200

    DOM Array Methods - For Each

--- 5. DOM Array Methods/script.js
+++ 5. DOM Array Methods/script.js
@@ -8,9 +8,6 @@ const calculateWealthBtn = document.getElementById('calculate-wealth');
 let data = [];
 //fetch random user and add money
 
-getRandomUser();
-getRandomUser();
-getRandomUser();
 async function getRandomUser() {
   const res = await fetch('https://randomuser.me/api');
   const data = await res.json();
@@ -20,11 +17,37 @@ async function getRandomUser() {
     name: `${user.name.first} ${user.name.last}`,
     money: Math.floor(Math.random() * 1000000),
   };
-  console.log(newUser);
+  addData(newUser);
 }
 
 //add new object to data array
 
 function addData(obj) {
   data.push(obj);
+
+  updateDOM();
 }
+
+//update DOM
+
+function updateDOM(providedData = data) {
+  //clear main div
+  main.innerHTML = '<h2><strong>Person</strong> Wealth</h2>';
+  providedData.forEach((item) => {
+    const element = document.createElement('div');
+    element.classList.add('person');
+    element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(
+      item.money
+    )}`;
+    main.appendChild(element);
+  });
+}
+
+//format number as money
+function formatMoney(number) {
+  return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,'); // 12,345.67
+}
+
+//event listeners
+
+addUserBtn.addEventListener('click', getRandomUser);
