8dc5d94b3b646de57120f9103c3902c788ae5401
Ivan Turnovski <ivanturnovski@gmail.com>
2020-11-12 15:51:04 +0100

    Hangman Game - DOM, SVG, Letter Press Event Handler

--- 7. Hangman Game DOM, SVG, Events/script.js
+++ 7. Hangman Game DOM, SVG, Events/script.js
@@ -18,22 +18,60 @@ const wrongLetters = [];
 //show hidden word
 function displayWord() {
   wordEl.innerHTML = `
-    ${selectedWord
-      .split('')
-      .map(
-        (letter) => `
-    <span class="letter">
-    ${correctLetters.includes(letter) ? letter : ''}
-    </span>
-    `
-      )
-      .join('')}
-  `;
+      ${selectedWord
+        .split('')
+        .map(
+          (letter) => `
+      <span class="letter">
+      ${correctLetters.includes(letter) ? letter : ''}
+      </span>
+      `
+        )
+        .join('')}
+    `;
   const innerWord = wordEl.innerText.replace(/\n/g, '');
   if (innerWord === selectedWord) {
     finalMessage.innerText = 'Congratulations! You Won';
     popup.style.display = 'flex';
   }
 }
+//Update the wrong letters
+function updateWrongLettersEl() {
+  console.log('update wrong');
+}
+
+//Update wrong letters elements
+function showNotification() {
+  notification.classList.add('show');
+  setTimeout(() => {
+    notification.classList.remove('show');
+  }, 2000);
+}
+
+//Key down letter press
+window.addEventListener('keydown', (e) => {
+  //   console.log(e.keyCode);
+
+  if ((e.keyCode >= 65) & (e.keyCode <= 90)) {
+    const letter = e.key;
+    if (selectedWord.includes(letter)) {
+      if (!correctLetters.includes(letter)) {
+        correctLetters.push(letter);
+
+        displayWord();
+      } else {
+        showNotification();
+      }
+    } else {
+      if (!wrongLetters.includes(letter)) {
+        wrongLetters.push(letter);
+
+        updateWrongLettersEl();
+      } else {
+        showNotification();
+      }
+    }
+  }
+});
 
 displayWord();
