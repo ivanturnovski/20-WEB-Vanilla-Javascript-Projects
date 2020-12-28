899b8eb7bd59fa6cf40c5037e2c0fa0ce1323480
Ivan Turnovski <ivanturnovski@gmail.com>
2020-12-28 07:43:29 +0100

    Display Balance, Income and Expense

--- 9. Expense Tracker/script.js
+++ 9. Expense Tracker/script.js
@@ -10,7 +10,7 @@ const dummyTransactions = [
   { id: 1, text: 'Flower', amount: -20 },
   { id: 2, text: 'Salary', amount: 300 },
   { id: 3, text: 'Book', amount: -10 },
-  { id: 4, text: 'Camera', amount: -150 },
+  { id: 4, text: 'Camera', amount: +150 },
 ];
 
 let transactions = dummyTransactions;
@@ -34,10 +34,30 @@ function addTransactionDOM(transaction) {
   list.appendChild(item);
 }
 
+//Update the balance income and expense
+
+function updateValues() {
+  const amounts = transactions.map((transaction) => transaction.amount);
+  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+  const income = amounts
+    .filter((item) => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+  console.log(income);
+  const expense = (
+    amounts.filter((item) => item < 0).reduce((acc, item) => (acc += item), 0) *
+    -1
+  ).toFixed(2);
+  console.log(expense);
+  balance.innerText = `$${total}`;
+  money_plus.innerText = `$${income}`;
+  money_minus.innerText = `$${expense}`;
+}
 //Init appendChild
 function init() {
   list.innerHTML = '';
   transactions.forEach(addTransactionDOM);
+  updateValues();
 }
 
 init();
